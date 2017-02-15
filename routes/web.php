cc67d7454266ce467bc4a39d223b5440001c7a19
Michael Czechowski <mail@dailysh.it>
2017-02-15 16:30:51 +0100

    added first routes draft

--- routes/web.php
+++ routes/web.php
@@ -29,11 +29,47 @@ Route::get('/', function () {
 |--------------------------------------------------------------------------
 |
 | Display only search formular with filtering options. If already started
-| enquiry
+| enquiry, load result page.
 |
 */
 Route::get('search', function () {
    return 'Suche';
 });
 
+Route::post('search', function () {
+    return 'Suche gestartet';
+});
+/*
+|--------------------------------------------------------------------------
+| Details
+|--------------------------------------------------------------------------
+|
+| Display details of entry and show editing and adding options.
+|
+*/
+Route::get('entry/{id}', function ($entryId) {
+    return 'Eintrag anzeigen:' . $entryId;
+})->where('id', '[0-9]+');
 
+Route::get('entry/{id}', function ($entryId) {
+    return 'Eintrag bearbeiten:' . $entryId;
+})->where('id', '[0-9]+');
+
+Route::put('entry', function () {
+    return 'Eintrag hinzufügen';
+});
+/*
+|--------------------------------------------------------------------------
+| Download
+|--------------------------------------------------------------------------
+|
+| Prepare bundled files for downloading one or more corpora entries
+|
+*/
+Route::get('download/{id}', function ($entryId) {
+    return 'Eintrag' . $entryId . ' herunterladen';
+})->where('id', '[0-9]+');
+
+Route::post('download', function ($entryId) {
+    return 'Mehrere Einträge herunterladen';
+});
