aedb9f3c8a5f706267b03d539e80cede38e359c8
Michael Czechowski <mail@dailysh.it>
2017-02-22 11:57:51 +0100

    adjusted sidebar and gray scales

--- resources/views/_components/navigation.blade.php
+++ resources/views/_components/navigation.blade.php
@@ -1,15 +1,34 @@
 <nav class="navigation">
-  <h5 class="navigation-header">Menü</h5>
+    <h5 class="navigation-header">DB Corpora</h5>
+    <!-- Main Navigation -->
+    <ul class="navigation-list">
+        <li class="navigation-list-item">
+            <a class="navigation-list-item-link" href="{{ route('dashboard') }}">Übersicht</a>
+        </li>
+        <li class="navigation-list-item">
+            <a class="navigation-list-item-link" href="{{ route('search') }}">Suche</a>
+        </li>
+        <li class="navigation-list-item">
+            <a class="navigation-list-item-link" href="{{ route('entry.create') }}">Neuer Eintrag</a>
+        </li>
+    </ul>
+    <!-- Last Activities -->
+    <ul class="navigation-list">
+      <li class="navigation-list-item-title">
+        Zuletzt gesucht...
+      </li>
+      <li class="navigation-list-item">
+        <a class="navigation-list-item-link" href="#">Platzhalter</a>
+      </li>
+    </ul>
+    <!-- Last Activities -->
+    <ul class="navigation-list">
+      <li class="navigation-list-item-title">
+        Zuletzt exportiert...
+      </li>
+      <li class="navigation-list-item">
+        <a class="navigation-list-item-link" href="#">Platzhalter</a>
+      </li>
+    </ul>
 
-  <ul class="navigation-list">
-    <li class="navigation-list-item">
-      <a class="navigation-list-item-link" href="{{ route('dashboard') }}">Übersicht</a>
-    </li>
-    <li class="navigation-list-item">
-      <a class="navigation-list-item-link" href="{{ route('search') }}">Suche</a>
-    </li>
-    <li class="navigation-list-item">
-      <a class="navigation-list-item-link" href="{{ route('entry.create') }}">Neuer Eintrag</a>
-    </li>
-  </ul>
 </nav>
