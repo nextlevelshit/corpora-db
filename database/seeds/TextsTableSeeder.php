55f2fae076a01c45d63c91d323615942a882f146
Michael Czechowski <mail@dailysh.it>
2017-02-23 18:21:26 +0100

    updated seeders

---
+++ database/seeds/TextsTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class TextsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory('App\Text', 10000)->create();
+    }
+}
