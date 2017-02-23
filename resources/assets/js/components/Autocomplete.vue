37f45954dcc87f430be269d93613c32c1cf83b52
Michael Czechowski <mail@dailysh.it>
2017-02-23 15:27:07 +0100

    finished autocomplete and added hidden id field

--- resources/assets/js/components/Autocomplete.vue
+++ resources/assets/js/components/Autocomplete.vue
@@ -1,19 +1,24 @@
 <template>
     <label class="autocomplete">
         {{ title }}
+        <input type="hidden"
+        v-model="id"
+        v-bind:name="name + '_id'" value="">
+
         <input type="text" class="autocomplete-input"
         v-model="term"
         v-bind:name="name"
         v-bind:placeholder="placeholder"
-        v-on:keyup="triggerAutocomplete">
+        v-on:keyup="triggerAutocomplete"
+        v-on:blur="">
 
         <ul class="autocomplete-list"
         v-bind:class="{ 'active': isActive }">
 
             <li class="autocomplete-list-item"
                 v-for="item in items"
-                v-on:click="selectFromAutocomplete">
-                {{ item }}
+                v-on:click="selectFromAutocomplete(item)">
+                {{ item.name }} <sup>[{{ item.id }}]</sup>
             </li>
         </ul>
     </label>
@@ -25,24 +30,31 @@
             return {
                 isActive: false,
                 term: null,
-                items: ['Schiller', 'Goethe']
+                id: null,
+                items: []
             }
         },
         props: ['title', 'name', 'placeholder'],
         methods: {
             triggerAutocomplete: function(event) {
-                console.log(this.term);
+                var url = '/api/' + this.name + '/' + this.term
 
-                this.$http.get('/api/authors', function(items) {
-                    this.$set('items', items);
+                this.$http.get(url).then(function(response) {
+                    this.items = response.body[0];
+                    // display autocomplete suggestions
+                    this.isActive = true;
                 });
-
-                // display autocomplete suggestions
-                this.isActive = true;
             },
 
-            selectFromAutocomplete: function(event) {
+            resetAutocomplete: function() {
                 this.isActive = false;
+                this.items = [];
+            },
+
+            selectFromAutocomplete: function(item) {
+                this.term = item.name;
+                this.id = item.id;
+                this.resetAutocomplete();
             }
         }
     }
