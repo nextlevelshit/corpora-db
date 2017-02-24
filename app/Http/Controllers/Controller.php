af53ef744342944ff8a840b869ba7ca2d35696cd
Michael Czechowski <mail@dailysh.it>
2017-02-24 18:11:14 +0100

    updated navigation sidebar

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,19 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use App\Entry;
+use View;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function __construct()
+    {
+        View::share('latestEntries', Entry::latest()->take(5)->get());
+        View::share('latestSearchRequests', '');
+        // View::share('latestSearchRequests', Search::latest()->take(5)->get());
+        View::share('latestExports', '');
+        // View::share('latestExports', Export::latest()->take(5)->get());
+    }
 }
--- resources/views/_components/navigation.blade.php
+++ resources/views/_components/navigation.blade.php
@@ -12,32 +12,56 @@
             <a class="navigation-list-item-link" href="{{ route('entry.create') }}">Neuer Eintrag</a>
         </li>
     </ul>
-    <!-- Last Activities -->
+    {{-- Latest Search Requests --}}
     <ul class="navigation-list">
-      <li class="navigation-list-item-title">
-        Zuletzt gesucht
-      </li>
-      <li class="navigation-list-item">
-        <a class="navigation-list-item-link" href="#">Platzhalter</a>
-      </li>
+        @if ($latestSearchRequests)
+            <ul class="navigation-list">
+                <li class="navigation-list-item-title">
+                    Zuletzt gesucht
+                </li>
+                @foreach ($latestSearchRequests as $request)
+                    <li class="navigation-list-item">
+                        <a class="navigation-list-item-link"
+                           href="{{ route('export.show', $request->id) }}">
+                           {{ str_limit($request->title, $limit=40, $end='...') }}
+                       </a>
+                    </li>
+                </ul>
+            @endforeach
+        @endif
     </ul>
-    <!-- Latest Activities -->
+    {{-- Latest Activities --}}
     <ul class="navigation-list">
-      <li class="navigation-list-item-title">
-        Zuletzt exportiert
-      </li>
-      <li class="navigation-list-item">
-        <a class="navigation-list-item-link" href="#">Platzhalter</a>
-      </li>
+        @if ($latestExports)
+            <ul class="navigation-list">
+                <li class="navigation-list-item-title">
+                    Zuletzt exportiert
+                </li>
+                @foreach ($latestExports as $export)
+                    <li class="navigation-list-item">
+                        <a class="navigation-list-item-link"
+                           href="{{ route('export.show', $export->id) }}">
+                           {{ str_limit($export->title, $limit=40, $end='...') }}
+                       </a>
+                    </li>
+                </ul>
+            @endforeach
+        @endif
     </ul>
-    <!-- Latest Entries -->
-    <ul class="navigation-list">
-      <li class="navigation-list-item-title">
-        Neuste Einträge
-      </li>
-      <li class="navigation-list-item">
-        <a class="navigation-list-item-link" href="#">Platzhalter</a>
-      </li>
-    </ul>
-
+    {{-- Latest Entries  --}}
+    @if ($latestEntries)
+        <ul class="navigation-list">
+            <li class="navigation-list-item-title">
+                Neuste Einträge
+            </li>
+            @foreach ($latestEntries as $entry)
+                <li class="navigation-list-item">
+                    <a class="navigation-list-item-link"
+                       href="{{ route('entry.show', $entry->id) }}">
+                       {{ str_limit($entry->title, $limit=40, $end='...') }}
+                   </a>
+                </li>
+            </ul>
+        @endforeach
+    @endif
 </nav>
