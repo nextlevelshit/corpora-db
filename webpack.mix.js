69c299f37a8d4de6465363199c21b038dccf2a35
Michael Czechowski <mail@dailysh.it>
2017-10-26 15:16:55 +0200

    fixed js bug

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,7 +12,8 @@ const { mix } = require('laravel-mix');
  */
 mix.sass('resources/assets/sass/app.scss', 'public/css');
 
-mix.combine([
+mix.js([
    'node_modules/jquery/dist/jquery.min.js',
-   'node_modules/foundation-sites/dist/js/foundation.min.js'
+   'node_modules/foundation-sites/dist/js/foundation.min.js',
+   'resources/assets/js/app.js'
 ], 'public/js/app.js');
