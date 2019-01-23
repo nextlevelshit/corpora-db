af5da99d7a4b01abc83dc7ecd37dc9faac2624c6
Michael Czechowski <mail@dailysh.it>
2019-01-23 10:25:07 +0100

    updated node modules

---
+++ resources/assets/js/components/MarkAll.vue
@@ -0,0 +1,30 @@
+<template>
+  <label class="search-list-item-check">
+    <input type="checkbox" 
+           class="search-list-item-check-input" 
+           name="markAllEntries">
+      <div class="search-list-item-check-trigger">
+        <i class="fa fa-check-square-o"></i>
+      </div>
+  </label>
+</template>
+
+<script>
+    export default {
+        directives: { },
+        data: function() {
+            return {
+                isEnabled: false
+            }
+        },
+        props: ['identfier'],
+        methods: {
+          enable: function() {
+            console.log('is enabling');
+          },
+          disable: function() {
+            console.log('is disabling');
+          }
+        }
+    }
+</script>
