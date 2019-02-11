480d39dd72c84e5c4b2e2ec5a78bbb0a57324edb
Michael Czechowski <mail@dailysh.it>
2019-02-11 18:43:43 +0100

    fixed result columns

--- resources/views/search/results/authorsItem.blade.php
+++ resources/views/search/results/authorsItem.blade.php
@@ -1,14 +1,14 @@
 <li class="search-list-item">
-  <div class="row">
+  {{--  <div class="row">
     <div class="small-1 column">
-      {{-- <label class="search-list-item-check">
+      <label class="search-list-item-check">
         <input type="checkbox" class="search-list-item-check-input" name="authors[]" value="{{ $author->id }}">
         <div class="search-list-item-check-trigger">
           <i class="fa fa-check-square-o"></i>
         </div>
-      </label> --}}
+      </label>
     </div>
-    <div class="small-11">
+    <div class="small-11">  --}}
       <h4 class="search-list-item-result">
         <a href="{{ route('author.show', $author->id) }}">
           {{ $author->name }}
@@ -26,6 +26,6 @@
           </li>
         @endif
       </ul>
-    </div>
-  </div>
+    {{--  </div>
+  </div>  --}}
 </li>
--- resources/views/search/results/texts.blade.php
+++ resources/views/search/results/texts.blade.php
@@ -1,40 +1,40 @@
 @if (count($texts) > 0)
   <ul class="search-list">
     @foreach ($texts as $text)
-    <li class="search-list-item">
-      <div class="row">
-        <div class="small-2 medium-1 column">
-          {{-- <label class="search-list-item-check">
-            <input type="checkbox" class="search-list-item-check-input" name="texts[]" value="{{ $text->id }}">
-            <div class="search-list-item-check-trigger">
-              <i class="fa fa-check-square-o"></i>
-            </div>
-          </label> --}}
-        </div>
-        <div class="small-10 medium-11">
-          <h4 class="search-list-item-result">
-            <a href="{{ route('entry.show', $text->entry->id) }}" target="_blank">
-              {{ $text->entry->title }}
-            </a>
-          </h4>
-          <ul class="search-list-item-info">
-            @if ($text->entry)
-              <li class="search-list-item-info-item">
-                Download: <a href="{{ $text->path }}" target="_blank">
-                  {{ $text->path }}
-                </a>
-              </li>
+      <li class="search-list-item">
+        {{--  <div class="row">
+          <div class="small-2 medium-1 column">
+            <label class="search-list-item-check">
+              <input type="checkbox" class="search-list-item-check-input" name="texts[]" value="{{ $text->id }}">
+              <div class="search-list-item-check-trigger">
+                <i class="fa fa-check-square-o"></i>
+              </div>
+            </label>
+          </div>
+          <div class="small-10 medium-11">  --}}
+            <h4 class="search-list-item-result">
+              <a href="{{ route('entry.show', $text->entry->id) }}" target="_blank">
+                {{ $text->entry->title }}
+              </a>
+            </h4>
+            <ul class="search-list-item-info">
+              @if ($text->entry)
+                <li class="search-list-item-info-item">
+                  Download: <a href="{{ $text->path }}" target="_blank">
+                    {{ $text->path }}
+                  </a>
+                </li>
+              @endif
+            </ul>
+            @if ($text->result)
+              {{-- <p>{{ str_limit($text->result, $limit=320, $end='...') }}</p> --}}
+              <p>
+                {!! str_replace($term, '<strong>'.$term.'</strong>', $text->result) !!}
+              </p>
             @endif
-          </ul>
-          @if ($text->result)
-            {{-- <p>{{ str_limit($text->result, $limit=320, $end='...') }}</p> --}}
-            <p>
-              {!! str_replace($term, '<strong>'.$term.'</strong>', $text->result) !!}
-            </p>
-          @endif
-        </div>
-      </div>
-    </li>
+          {{--  </div>
+        </div>  --}}
+      </li>
     @endforeach
   </ul>
 @else
